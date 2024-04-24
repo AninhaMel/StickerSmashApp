25ecf55d6bfe05a286412223756c006495baa97b
AninhaMel <ana.silva5134@etec.sp.gov.br>
2024-04-24 12:16:40 -0300

    Feito o emoji

--- StickerSmash/App.js
+++ StickerSmash/App.js
@@ -9,6 +9,7 @@ import * as ImagePicker from 'expo-image-picker';
 import EmojiPicker from './src/components/EmojiPicker';
 import EmojiList from './src/components/EmojiList';
 import EmojiSticker from './src/components/EmojiSticker';
+import {GestureHandlerRootView} from "react-native-gesture-handler";
 
 const PlaceHolderImage = require('./assets/images/background-image.png');
 
@@ -47,7 +48,7 @@ export default function App() {
     }
   }; 
   return (
-    <View style={styles.container}>
+    <GestureHandlerRootView style={styles.container}>
       <View style={styles.ImageContainer}>
       <ImageViewer 
       placeholderImageSource={PlaceHolderImage}
@@ -81,7 +82,7 @@ export default function App() {
           <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
         </EmojiPicker>
       <StatusBar style="auto" />
-    </View>
+    </GestureHandlerRootView>
   );
 }
 
